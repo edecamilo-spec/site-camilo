35818892394ac1ccab3e3c86ad694631805dc18b
edecamilo-spec <edecamilo@gmail.com>
2025-10-16 19:10:38 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export default function Page() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-900">
